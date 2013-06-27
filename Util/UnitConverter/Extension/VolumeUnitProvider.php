8e5fc3cf6ebce6faf2bccf70581a06ca8c14ba36
Cedric Deschenes <cedricdeschenes@gmail.com>
2013-06-27 11:29:56 -0400

    Added ft ratios, cleaned prefix for the DistanceUnitProvider and added the VolumeUnitProvider

--- Util/UnitConverter/Extension/VolumeUnitProvider.php
+++ Util/UnitConverter/Extension/VolumeUnitProvider.php
@@ -9,16 +9,16 @@ use BCC\ExtraToolsBundle\Util\UnitConverter\BaseRatioUnitProvider;
  *
  * @author michel
  */
-class DistanceUnitProvider extends BaseRatioUnitProvider {
-    
+class VolumeUnitProvider extends BaseRatioUnitProvider {
+
     /**
      * {@inheritDoc}
      */
     public function getRatios() {
         return array(
-            'm'  => 1,
-            'in'  => 0.0254,
-            'ft'  => 0.3048
+            'm3'  => 1,
+            'in3'  => 0.0610,
+            'ft3'  => 0.0283
         );
     }
 
@@ -31,7 +31,6 @@ class DistanceUnitProvider extends BaseRatioUnitProvider {
             'da' => 10,
             'd'  => 0.1,
             'c'  => 0.01,
-            'm'  => 0.001,
             'n'  => 0.000000001,
         );
     }
@@ -47,6 +46,6 @@ class DistanceUnitProvider extends BaseRatioUnitProvider {
      * {@inheritDoc}
      */
     public function getUnit() {
-        return self::LENGTH;
+        return self::VOLUME;
     }
 }
